58ed0145edc8c396204a04f9c68110842ef49d8b
Noo6 <72285529+aNoo6@users.noreply.github.com>
2022-06-04 15:02:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # 设置默认IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-ed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # 设置默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
