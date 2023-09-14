ea1f05a078543b718e2e331c3fee31054ebb465b
Noo6 <72285529+No06@users.noreply.github.com>
2023-09-14 22:47:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,5 +25,3 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
